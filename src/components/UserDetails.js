4a48ee4df5d54aa250f341f063b6c9db2bc009d3
JPTheWeatherMan <JarrettPHale@gmail.com>
2019-09-29 15:22:02 -0400

    Skeleton for UserDetails page

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,21 +1,37 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, Fragment } from 'react';
 import R6Context from '../r6Context/R6Context';
-import { Typography } from '@material-ui/core';
+import { Typography, Card, CardContent } from '@material-ui/core';
 
 const UserDetails = ({ match }) => {
   const r6context = useContext(R6Context);
-  const { user, getUser } = r6context;
+  const { user, getUser, loading } = r6context;
 
-  useEffect(async () => {
-    await getUser(match.params.playerid); // eslint-disable-next-line
+  useEffect(() => {
+    async function fetchData() {
+      await getUser(match.params.playerid);
+    }
+    fetchData(); // eslint-disable-next-line
   }, [match.params.playerid]); // I'm having a lot of trouble with dependency array
 
-  console.log(user);
-  return (
-    <div>
-      <h1> Page</h1>
-    </div>
-  );
+  if (loading) {
+    return <div>loading</div>;
+  } else {
+    console.log(user);
+    return (
+      <Fragment>
+        <Card>
+          <CardHeader-title>
+            <Typography>{user.p_name}</Typography>
+          </CardHeader-title>
+          <CardContent>
+            <Typography>Level: {user.p_level}</Typography>
+            <Typography>Kill/Death: {user.kd}</Typography>
+            <Typography>Platform: {user.p_platform}</Typography>
+          </CardContent>
+        </Card>
+      </Fragment>
+    );
+  }
 };
 
 export default UserDetails;
