140d9e36cd1385d7a473b36a22114dbe6cb71be4
JPTheWeatherMan <JarrettPHale@gmail.com>
2019-10-05 21:33:52 -0400

    Added breakpoints

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import BackgroundImage from '../img/rainbow.jpg';
+import { withStyles } from '@material-ui/core/styles';
 
-const Background = () => {
-  let styles = {
-    backgroundStyle: {
+const styles = theme => ({
+  root: {
+    padding: theme.spacing(1),
+    [theme.breakpoints.down('md')]: {
       position: 'fixed',
+      margin: 'auto',
+      padding: 0,
+      height: '100%',
+      width: 'auto',
+      zIndex: -1,
+      backgroundSize: 'center center',
+      backgroundPosition: 'center',
+      opacity: 1,
+      overflow: 'hidden'
+    },
+    [theme.breakpoints.up('md')]: {
+      position: 'fixed',
+      margin: 'auto',
+      padding: 0,
       height: '100%',
       width: '100%',
       zIndex: -1,
-      backgroundSize: 'cover',
-      backgroundPosition: 'cover',
-      opacity: 1
+      backgroundSize: 'center center',
+      backgroundPosition: 'center',
+      opacity: 1,
+      overflow: 'hidden'
     }
-  };
+  }
+});
+
+const Background = props => {
+  const { classes } = props;
 
   return (
     <img
-      style={styles.backgroundStyle}
+      className={classes.root}
       src={BackgroundImage}
       alt={'background'}
     ></img>
   );
 };
 
-export default Background;
+export default withStyles(styles)(Background);
+
+// backgroundStyle: {
+//   position: 'fixed',
+//   height: 'auto',
+//   width: 'auto',
+//   zIndex: -1,
+//   'background-Size': 'center center',
+//   'background-Position': 'center',
+//   opacity: 1,
+//   overflow: 'hidden'
+// }
