ca7ee2b0a0e396b9b51d529c0e4c554dc3b96e47
JPTheWeatherMan <JarrettPHale@gmail.com>
2019-10-07 13:58:15 -0400

    Added responsive styling

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,28 +2,43 @@ import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import R6Context from '../r6Context/R6Context';
 import Loader from '../components/Loader';
+import { withStyles } from '@material-ui/core/styles';
 
-const Users = () => {
-  const styles = {
-    layoutStyle: {
+const styles = theme => ({
+  root: {
+    [theme.breakpoints.down('md')]: {
+      width: '60%',
+      margin: '0 auto',
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    [theme.breakpoints.up('md')]: {
       width: '60%',
       margin: '0 auto',
       display: 'flex',
       flexDirection: 'row',
       flexWrap: 'wrap',
-      justifyContent: 'center'
+      justifyContent: 'center',
+      alignItems: 'center'
     }
-  };
+  }
+});
 
+const Users = props => {
   const r6Context = useContext(R6Context);
 
   const { loading, users } = r6Context;
+
+  const { classes } = props;
 
   if (loading) {
     return <Loader />;
   } else {
     return (
-      <div style={styles.layoutStyle}>
+      <div className={classes.root}>
         {users.map(user => (
           <UserItem key={user.p_id} user={user} />
         ))}
@@ -32,4 +47,4 @@ const Users = () => {
   }
 };
 
-export default Users;
+export default withStyles(styles)(Users);
