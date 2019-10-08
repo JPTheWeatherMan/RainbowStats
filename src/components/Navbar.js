1cd926f0c194f4aae22e0989661dceee525f33f2
JPTheWeatherMan <JarrettPHale@gmail.com>
2019-10-07 21:24:03 -0400

    Responsive styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import { AppBar, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-const Navbar = () => {
-  const styles = {
-    mainStyle: {
-      width: '100%',
-      height: '10vh',
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: '10vh',
+    display: 'flex',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  navlinks: {
+    [theme.breakpoints.down('md')]: {
+      width: '65%',
       display: 'flex',
-      justifyContent: 'space-around',
       flexDirection: 'row',
-      alignItems: 'center'
+      justifyContent: 'space-around'
     },
-    navLinkStyle: {
+    [theme.breakpoints.up('md')]: {
       width: '15%',
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'space-around'
     }
-  };
+  }
+});
+
+const Navbar = props => {
+  const { classes } = props;
 
   return (
-    <AppBar style={styles.mainStyle} position='sticky'>
-      <h1>JP</h1>
-      <ul style={styles.navLinkStyle}>
+    <AppBar className={classes.root} position='sticky'>
+      <h1>R6</h1>
+      <ul className={classes.navlinks}>
         <Button variant='contained' size='medium' href='/'>
           Home
         </Button>
@@ -34,4 +45,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
