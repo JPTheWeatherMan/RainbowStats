47198acb0937a0ef5d137828241f632ef2cfdb43
unknown <jarrettphale@gmail.com>
2019-10-15 21:04:59 -0400

    completed about page and 404

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,31 @@
 import React, { Fragment } from 'react';
 import Navbar from '../components/Navbar';
 import Background from '../components/Background';
-import { Card, Typography, CardContent } from '@material-ui/core';
+import { Card, Typography, CardContent, withStyles } from '@material-ui/core';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-const About = () => {
+const styles = theme => ({
+  card: {
+    [theme.breakpoints.down('md')]: {
+      margin: '20px auto',
+      textAlign: 'center',
+      width: '80%'
+    },
+    [theme.breakpoints.up('md')]: {
+      margin: '20px auto',
+      textAlign: 'center',
+      width: '60%'
+    }
+  }
+});
+
+const About = props => {
+  const { classes } = props;
   return (
     <Fragment>
       <Navbar />
       <Background />
-      <Card>
+      <Card className={classes.card}>
         <CardHeader-title>
           <Typography variant='h3' align='center' color='textPrimary'>
             About R6 Stats
@@ -17,17 +33,29 @@ const About = () => {
         </CardHeader-title>
         <CardContent>
           <Typography variant='h5' align='center' color='textPrimary'>
-            This application was built by Jarrett Hale
+            This application was built by Jarrett Hale and is in version 1.0
           </Typography>
           <Typography variant='h5' align='center' color='textPrimary'>
             Links:
           </Typography>
-          <FaGithub size='40' color='#3f51b5' />
-          <FaLinkedin size='40' color='#3f51b5' />
+          <a
+            href='https://github.com/JPTheWeatherMan'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub size='40' color='#3f51b5' />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/jarrett-hale-a82253189/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin size='40' color='#3f51b5' />
+          </a>
         </CardContent>
       </Card>
     </Fragment>
   );
 };
 
-export default About;
+export default withStyles(styles)(About);
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,41 @@
 import React, { Fragment } from 'react';
+import Navbar from '../components/Navbar';
 import Background from '../components/Background';
+import { Card, Typography, CardContent, withStyles } from '@material-ui/core';
 
-const NotFound = () => {
+const styles = theme => ({
+  card: {
+    [theme.breakpoints.down('md')]: {
+      margin: '20px auto',
+      textAlign: 'center',
+      width: '80%'
+    },
+    [theme.breakpoints.up('md')]: {
+      margin: '20px auto',
+      textAlign: 'center',
+      width: '60%'
+    }
+  }
+});
+
+const NotFound = props => {
+  const { classes } = props;
   return (
     <Fragment>
+      <Navbar />
       <Background />
-      <div>404</div>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant='h5' align='center' color='textPrimary'>
+            404
+          </Typography>
+          <Typography variant='h5' align='center' color='textPrimary'>
+            Not Found
+          </Typography>
+        </CardContent>
+      </Card>
     </Fragment>
   );
 };
 
-export default NotFound;
+export default withStyles(styles)(NotFound);
